35880209698d2d132771d9e0e1140885272eb43b
วัจน์กร จงกลาง < >
2016-08-23 17:38:05 +0700

    sevenDialog

--- src/app/main/apps/employee/dialogs/compose/compose-dialog.controller.js
+++ src/app/main/apps/employee/dialogs/compose/compose-dialog.controller.js
@@ -1,5 +1,4 @@
-(function ()
-{
+(function() {
     'use strict';
 
     angular
@@ -7,8 +6,7 @@
         .controller('ComposeDialogController', ComposeDialogController);
 
     /** @ngInject */
-    function ComposeDialogController($mdDialog, selectedMail, $scope)
-    {
+    function ComposeDialogController($mdDialog, selectedMail, $scope) {
         var vm = this;
 
         // Data
@@ -20,8 +18,7 @@
         vm.hiddenBCC = true;
 
         // If replying
-        if ( angular.isDefined(selectedMail) )
-        {
+        if (angular.isDefined(selectedMail)) {
             vm.form.to = selectedMail.from.email;
             vm.form.subject = 'RE: ' + selectedMail.subject;
             vm.form.message = '<blockquote>' + selectedMail.message + '</blockquote>';
@@ -33,9 +30,9 @@
 
 
         //////////
+        $scope.active = true;
 
-        function closeDialog()
-        {
+        function closeDialog() {
             $mdDialog.hide();
         }
         vm.stepper = {
@@ -46,12 +43,11 @@
 
         vm.basicForm = {};
         vm.formWizard = {};
-        vm.sex = [{"name":"Male"},{"name":"Famale"}];
+        vm.sex = [{ "name": "Male" }, { "name": "Famale" }];
         vm.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-        'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-        'WY').split(' ').map(function (state)
-        {
-            return {abbrev: state};
+            'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+            'WY').split(' ').map(function(state) {
+            return { abbrev: state };
         });
 
         // Methods
@@ -81,45 +77,45 @@
         //     $scope.family.Occupation =''; 
         //     $scope.family.tel =''; 
         // }
-        function addNewList() { 
-            vm.family.push({ 'nameFm': vm.family.nameFm, 
-                                 'LastNameFm' : vm.family.LastNameFm, 
-                                 'age': vm.family.age, 
-                                 'related': vm.family.related , 
-                                 'Occupation': vm.family.Occupation , 
-                                 'tel': vm.family.tel }); 
-            vm.family.nameFm = ''; 
-            vm.family.LastNameFm = ''; 
-            vm.family.age =''; 
-            vm.family.related =''; 
-            vm.family.Occupation =''; 
-            vm.family.tel =''; 
+        function addNewList() {
+            vm.family.push({
+                'nameFm': vm.family.nameFm,
+                'LastNameFm': vm.family.LastNameFm,
+                'age': vm.family.age,
+                'related': vm.family.related,
+                'Occupation': vm.family.Occupation,
+                'tel': vm.family.tel
+            });
+            vm.family.nameFm = '';
+            vm.family.LastNameFm = '';
+            vm.family.age = '';
+            vm.family.related = '';
+            vm.family.Occupation = '';
+            vm.family.tel = '';
         }
-        function submitStepper(ev)
-        {
+
+        function submitStepper(ev) {
             // You can do an API call here to send the form to your server
 
             // Show the sent data.. you can delete this safely.
             $mdDialog.show({
-                controller         : function ($scope, $mdDialog, formWizardData)
-                {
+                controller: function($scope, $mdDialog, formWizardData) {
                     $scope.formWizardData = formWizardData;
-                    $scope.closeDialog = function ()
-                    {
+                    $scope.closeDialog = function() {
                         $mdDialog.hide();
                     }
                 },
-                template           : '<md-dialog>' +
-                '  <md-dialog-content><h1>You have sent the form with the following data</h1><div><pre>{{formWizardData | json}}</pre></div></md-dialog-content>' +
-                '  <md-dialog-actions>' +
-                '    <md-button ng-click="closeDialog()" class="md-primary">' +
-                '      Close' +
-                '    </md-button>' +
-                '  </md-dialog-actions>' +
-                '</md-dialog>',
-                parent             : angular.element('body'),
-                targetEvent        : ev,
-                locals             : {
+                template: '<md-dialog>' +
+                    '  <md-dialog-content><h1>You have sent the form with the following data</h1><div><pre>{{formWizardData | json}}</pre></div></md-dialog-content>' +
+                    '  <md-dialog-actions>' +
+                    '    <md-button ng-click="closeDialog()" class="md-primary">' +
+                    '      Close' +
+                    '    </md-button>' +
+                    '  </md-dialog-actions>' +
+                    '</md-dialog>',
+                parent: angular.element('body'),
+                targetEvent: ev,
+                locals: {
                     formWizardData: vm.stepper
                 },
                 clickOutsideToClose: true
@@ -136,31 +132,28 @@
         /**
          * Send form
          */
-        function sendForm(ev)
-        {
+        function sendForm(ev) {
             // You can do an API call here to send the form to your server
 
             // Show the sent data.. you can delete this safely.
             $mdDialog.show({
-                controller         : function ($scope, $mdDialog, formWizardData)
-                {
+                controller: function($scope, $mdDialog, formWizardData) {
                     $scope.formWizardData = formWizardData;
-                    $scope.closeDialog = function ()
-                    {
+                    $scope.closeDialog = function() {
                         $mdDialog.hide();
                     }
                 },
-                template           : '<md-dialog>' +
-                '  <md-dialog-content><h1>You have sent the form with the following data</h1><div><pre>{{formWizardData | json}}</pre></div></md-dialog-content>' +
-                '  <md-dialog-actions>' +
-                '    <md-button ng-click="closeDialog()" class="md-primary">' +
-                '      Close' +
-                '    </md-button>' +
-                '  </md-dialog-actions>' +
-                '</md-dialog>',
-                parent             : angular.element('body'),
-                targetEvent        : ev,
-                locals             : {
+                template: '<md-dialog>' +
+                    '  <md-dialog-content><h1>You have sent the form with the following data</h1><div><pre>{{formWizardData | json}}</pre></div></md-dialog-content>' +
+                    '  <md-dialog-actions>' +
+                    '    <md-button ng-click="closeDialog()" class="md-primary">' +
+                    '      Close' +
+                    '    </md-button>' +
+                    '  </md-dialog-actions>' +
+                    '</md-dialog>',
+                parent: angular.element('body'),
+                targetEvent: ev,
+                locals: {
                     formWizardData: vm.formWizard
                 },
                 clickOutsideToClose: true
@@ -170,5 +163,5 @@
             vm.formWizard = {};
         }
     }
-  
+
 })();
