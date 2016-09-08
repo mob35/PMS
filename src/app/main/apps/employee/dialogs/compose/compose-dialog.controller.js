02078b26f57053eee8c830ca4a2910ae281d67cf
nattawee <nongnatawee@hotmail.com>
2016-09-08 17:24:46 +0700

    CallCrtEmpPowerFull

--- src/app/main/apps/employee/dialogs/compose/compose-dialog.controller.js
+++ src/app/main/apps/employee/dialogs/compose/compose-dialog.controller.js
@@ -8,181 +8,30 @@
     /** @ngInject */
     function ComposeDialogController($mdDialog, selectedMail, $scope) {
         $scope.newEmp = {
-            "EmpID": "1",
             "PersonalInfo": {
-                "EmpID": "1",
-                "FirstNameTH": "",
-                "LastNameTH": "",
-                "FirstNameEN": "",
-                "LastNameEN": "",
-                "Position": "",
-                "Salary": "",
-                "PresentAddress": "",
-                "Moo": "",
-                "District": "",
-                "Amphur": "",
-                "Province": "",
-                "Postcode": "",
-                "Tel": "",
-                "Moblie": "",
-                "Email": "",
-                "Living": "",
-                "Birthdate": "",
-                "Age": "",
-                "Race": "",
-                "Nationality": "",
-                "Religion": "",
-                "IDCard": "",
-                "IDCardExp": "",
-                "Height": "",
-                "Weight": "",
-                "Military": "",
-                "Marital": "",
-                "Sex": "Male",
-                "Img": "",
-                "UnderlyingDisease": "",
-                "Account": "",
-                "ObjectState": "",
-                "Created": "",
-                "CreatedBy": "",
-                "Updated": "",
-                "UpdatedBy": ""
             },
             "FamilyInfo": [],
             "EducationInfo": [],
             "WorkExperienceInfo": [],
             "LanguageInfo": [],
             "SpecialInfo": [{
-                "SpeID": 1,
-                "EmpID": "1",
-                "TypingSI": 'true',
-                "TypingTHSI": 4,
-                "TypingENSI": 5,
-                "ComputerSI": true,
-                "ComputerDescSI": "sample string 7",
-                "DrivingSI": true,
-                "DrivingNoSI": "sample string 9",
-                "OfficeMachineSI": "sample string 10",
-                "HobbiceSI": "sample string 11",
-                "SportSI": "sample string 12",
-                "KnowledgeSI": "sample string 13",
-                "OtherSI": "sample string 14",
-                "ObjectState": 0,
-                "Created": "2016-07-26T19:15:28.8719377+07:00",
-                "CreatedBy": "sample string 15",
-                "Updated": "2016-07-26T19:15:28.8719377+07:00",
-                "UpdatedBy": "sample string 16"
+
+                "TypingSI": 'false',
+                "ComputerSI": 'false',
+                "DrivingSI": 'false'
+                
+
             }],
-            "OtherInfo": {
-                "EmpID": "1",
-                "WorkCountryOI": true,
-                "WorkCountryDescOI": "sample string 3",
-                "FirtNameOI": "sample string 4",
-                "LastNameOI": "sample string 5",
-                "RelatedOI": "sample string 6",
-                "AddressOI": "sample string 7",
-                "TelOI": "sample string 8",
-                "SourceInfoOI": "sample string 9",
-                "ContagiousFlagOI": true,
-                "ContagiousDescOI": "sample string 11",
-                "FirtNameWorkRelativeOI": "sample string 12",
-                "LastNameWorkRelativeOI": "sample string 13",
-                "IntroduceYoureSelfOI": "sample string 14",
-                "ObjectState": 0,
-                "Created": "2016-07-26T18:53:36.01",
-                "CreatedBy": "sample string 15",
-                "Updated": "2016-07-26T18:53:36.01",
-                "UpdatedBy": "sample string 16"
-            },
-            "RegardInfo": {
-                "EmpID": "1",
-                "PositionRI": "sample string 2",
-                "SalaryRI": 3,
-                "StartDateRI": "2016-07-26T18:53:36.01",
-                "ObjectState": 0,
-                "Created": "2016-07-26T18:53:36.01",
-                "CreatedBy": "sample string 4",
-                "Updated": "2016-07-26T18:53:36.01",
-                "UpdatedBy": "sample string 5"
-            },
-            "BenefitsInfo": [{
-                "BnID": 1,
-                "EmpID": "1",
-                "NameBN": "sample string 3",
-                "DetailBN": "sample string 4",
-                "ObjectState": 0,
-                "Created": "2016-07-26T18:53:36.01",
-                "CreatedBy": "sample string 5",
-                "Updated": "2016-07-26T18:53:36.01",
-                "UpdatedBy": "sample string 6"
-            }, {
-                "BnID": 2,
-                "EmpID": "1",
-                "NameBN": "sample string 3",
-                "DetailBN": "sample string 4",
-                "ObjectState": 0,
-                "Created": "2016-07-26T18:53:36.01",
-                "CreatedBy": "sample string 5",
-                "Updated": "2016-07-26T18:53:36.01",
-                "UpdatedBy": "sample string 6"
-            }],
-            "CertificateInfo": {
-                "EmpID": "1",
-                "DateRecordCI": "2016-07-26T19:15:28.873",
-                "FirstNameCI": "sample string 2",
-                "LastNameCI": "sample string 3",
-                "PositionCI": "sample string 4",
-                "DateStartCI": "2016-07-26T19:15:28.873",
-                "SalaryCI": 5,
-                "DatePrintCI": "2016-07-26T19:15:28.873",
-                "BossSign": "sample string 6",
-                "ObjectState": 0,
-                "Created": "2016-07-26T19:15:28.873",
-                "CreatedBy": "sample string 7",
-                "Updated": "2016-07-26T19:15:28.873",
-                "UpdatedBy": "sample string 8"
-            },
-            "EvaluationInfo": {
-                "EmpID": "1",
-                "DateConfirmedEV": "2016-07-26T19:35:02.69",
-                "JobLearningEV": 6,
-                "JobKnowledge_SkillEV": 7,
-                "AdaptationEV": 8,
-                "RulesRespectEV": 9,
-                "CommunicationEV": 10,
-                "AttitudeEV": 11,
-                "Planning_FollowingEV": 12,
-                "Decision_SolvingEV": 13,
-                "ResponsibilityEV": 14,
-                "CooperationEV": 15,
-                "SumP5EV": 16,
-                "SumP4EV": 17,
-                "SumP3EV": 18,
-                "SumP2EV": 19,
-                "SumP1EV": 20,
-                "TotalScoreEV": 21,
-                "PercentEV": 22,
-                "OtherEV": "sample string 23",
-                "EvaluationTypeEV": "sample string 24",
-                "ObjectState": 0,
-                "Created": "2016-07-26T19:35:02.69",
-                "CreatedBy": "sample string 25",
-                "Updated": "2016-07-26T19:35:02.69",
-                "UpdatedBy": "sample string 26"
-            },
-            "IncreaseInfo": [],
-            "DeductionInfo": [],
-            "SilpInfo": [],
-            "ObjectState": 0,
-            "Created": "2016-09-06T16:49:39.14",
-            "CreatedBy": "sample string 2",
-            "Updated": "2016-07-26T18:53:36.01",
-            "UpdatedBy": "sample string 3"
+            "OtherInfo": {}
         };
 
         console.log($scope.newEmp.SpecialInfo);
-        $scope.done = function () {
+        $scope.done = function() {
             console.log($scope.newEmp);
+        };
+        $scope.sendForm = function() {
+            console.log(JSON.stringify($scope.newEmp));
+            $mdDialog.hide();
         };
         var vm = this;
 
@@ -242,7 +91,9 @@
         $scope.typeMilitary = ['Exempted', 'Served', 'Not Yet'];
         $scope.typeMarital = ['Single', 'Married', 'Widowed', 'Separated'];
         $scope.typeSpecial = ['true', 'false'];
+
         // Methods
+
 
         $scope.family = {
             "FirstNameFI": "",
